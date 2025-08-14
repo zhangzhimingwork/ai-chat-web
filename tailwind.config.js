203f740f56c58ea537a8dbba0e78ca939fced61a
zhangzhimingwork <56144141+zhangzhimingwork@users.noreply.github.com>
2025-08-14 21:23:54 +0800

    修复Tailwind配置 - 移除typography插件依赖并添加内置样式

--- tailwind.config.js
+++ tailwind.config.js
@@ -49,9 +49,129 @@ module.exports = {
       maxWidth: {
         '8xl': '88rem',
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: 'none',
+            color: 'inherit',
+            a: {
+              color: 'inherit',
+              textDecoration: 'underline',
+              fontWeight: '500',
+            },
+            '[class~="lead"]': {
+              color: 'inherit',
+            },
+            strong: {
+              color: 'inherit',
+              fontWeight: '600',
+            },
+            'ol[type="A"]': {
+              '--list-counter-style': 'upper-alpha',
+            },
+            'ol[type="a"]': {
+              '--list-counter-style': 'lower-alpha',
+            },
+            'ol[type="A" s]': {
+              '--list-counter-style': 'upper-alpha',
+            },
+            'ol[type="a" s]': {
+              '--list-counter-style': 'lower-alpha',
+            },
+            'ol[type="I"]': {
+              '--list-counter-style': 'upper-roman',
+            },
+            'ol[type="i"]': {
+              '--list-counter-style': 'lower-roman',
+            },
+            'ol[type="I" s]': {
+              '--list-counter-style': 'upper-roman',
+            },
+            'ol[type="i" s]': {
+              '--list-counter-style': 'lower-roman',
+            },
+            'ol[type="1"]': {
+              '--list-counter-style': 'decimal',
+            },
+            'ol > li': {
+              position: 'relative',
+            },
+            'ol > li::before': {
+              content: 'counter(list-item, var(--list-counter-style, decimal)) "."',
+              position: 'absolute',
+              fontWeight: '400',
+              color: 'inherit',
+            },
+            'ul > li': {
+              position: 'relative',
+            },
+            'ul > li::before': {
+              content: '""',
+              position: 'absolute',
+              backgroundColor: 'currentColor',
+              borderRadius: '50%',
+            },
+            hr: {
+              borderColor: 'inherit',
+              borderTopWidth: 1,
+              marginTop: '3em',
+              marginBottom: '3em',
+            },
+            blockquote: {
+              fontWeight: '500',
+              fontStyle: 'italic',
+              color: 'inherit',
+              borderLeftWidth: '0.25rem',
+              borderLeftColor: 'inherit',
+              quotes: '"\\201C""\\201D""\\2018""\\2019"',
+              marginTop: '1.6em',
+              marginBottom: '1.6em',
+              paddingLeft: '1em',
+            },
+            h1: {
+              color: 'inherit',
+              fontWeight: '800',
+            },
+            h2: {
+              color: 'inherit',
+              fontWeight: '700',
+            },
+            h3: {
+              color: 'inherit',
+              fontWeight: '600',
+            },
+            h4: {
+              color: 'inherit',
+              fontWeight: '600',
+            },
+            'figure figcaption': {
+              color: 'inherit',
+            },
+            code: {
+              color: 'inherit',
+              fontWeight: '600',
+            },
+            'a code': {
+              color: 'inherit',
+            },
+            pre: {
+              color: 'inherit',
+              backgroundColor: 'transparent',
+            },
+            thead: {
+              color: 'inherit',
+              fontWeight: '600',
+              borderBottomWidth: '1px',
+              borderBottomColor: 'inherit',
+            },
+            'tbody tr': {
+              borderBottomWidth: '1px',
+              borderBottomColor: 'inherit',
+            },
+          },
+        },
+      },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [],
 }
